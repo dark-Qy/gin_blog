732f0857fd61202386ed7430dc9606e208f11373
dark-Qy <2454271718@qq.com>
2024-03-27 20:06:50 +0800

    修改部分数据结构

--- models/Comment.go
+++ models/Comment.go
@@ -2,6 +2,7 @@ package models
 
 // Comment 定义评论结构体
 type Comment struct {
-	UserName User   `json:"userName"`
-	Content  string `json:"content"`
+	CommentId int    `json:"commentId"`
+	UserName  User   `json:"userName"`
+	Content   string `json:"content"`
 }
